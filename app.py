8cd2504d24df51259630608137ed7f7a7e64d419
Popuri Keerthika <Keerthikapopuri@gmail.com>
2024-09-25 14:43:43 +0530

    done

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def home():
 @app.route("/train", methods=['GET','POST'])
 @cross_origin()
 def trainRoute():
-    os.system("dvc repro")
+    #os.system("dvc repro")
+    os.system("python main.py")
     return "Training done successfully!"
 
 
